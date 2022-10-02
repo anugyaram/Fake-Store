fe5496d0c00a140e18e5981c9c5e61a98bb5308d
Anugya Ram <anugyaram08@gmail.com>
2022-10-02 16:55:01 +0530

    Calling api

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
+const result = document.getElementById('result')
+const listItems = []
+
+getData()
+
+async function getData(){
+    const res = await fetch('https://fakestoreapi.com/products')
+
+    const data = await res.json()
+
+    data.forEach((user) => {
+        console.log(user)
+        const li = document.createElement('li')
+
+        listItems.push(li)
+
+        li.innerHTML = `
+            <p><b>Category:</b> ${user.category}</p>
+          <img src="${user.image}" alt="${user.title}">
+          <div class="user-info">
+            <h2>${user.title}</h2>
+            <p>${user.description}</p>
+            <p><b>Cost:</b> ${user.price}</p>
+            <button class="btn">Add To Cart</button>
+          </div>
+        `
+        result.appendChild(li)
+    })
+}
 
